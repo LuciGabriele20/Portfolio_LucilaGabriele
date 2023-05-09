d028b59b0f2681098675870011a9b94675f650c0
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-09 18:37:04 -0300

    portfolio

--- PortfolioArg/src/app/app.module.ts
+++ PortfolioArg/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoargComponent } from './components/logoarg/logoarg.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoargComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
