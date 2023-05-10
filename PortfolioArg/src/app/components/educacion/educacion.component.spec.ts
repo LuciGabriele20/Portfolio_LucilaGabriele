4c39421fa350c9316235a9247680b69bb1ec1038
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-10 19:01:53 -0300

    educacion

---
+++ PortfolioArg/src/app/components/educacion/educacion.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EducacionComponent } from './educacion.component';
+
+describe('EducacionComponent', () => {
+  let component: EducacionComponent;
+  let fixture: ComponentFixture<EducacionComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EducacionComponent]
+    });
+    fixture = TestBed.createComponent(EducacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
