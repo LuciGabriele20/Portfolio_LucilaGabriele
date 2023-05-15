d7c2867b91d495d4d36ebdc83ce5c026421381f4
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-15 04:27:13 -0300

    acercaDe

--- PortfolioArg/FrontEnd/src/app/components/logoarg/logoarg.component.ts
+++ PortfolioArg/FrontEnd/src/app/components/logoarg/logoarg.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-logoarg',
   templateUrl: './logoarg.component.html',
   styleUrls: ['./logoarg.component.css']
 })
-export class LogoargComponent {
+export class LogoargComponent implements OnInit {
+  
+       constructor(private router:Router) {}
 
+       ngOnInit(): void {
+       }
+
+       login(){
+        this.router.navigate(['/login'])
+       }
+      }
 }
