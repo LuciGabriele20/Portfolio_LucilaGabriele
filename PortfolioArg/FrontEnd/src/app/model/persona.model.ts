7c19a75b5faf6d7095e25cebb394c9e034d6d646
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-15 04:18:54 -0300

    model persona

---
+++ PortfolioArg/FrontEnd/src/app/model/persona.model.ts
@@ -0,0 +1,11 @@
+export class persona{
+    id ?: number;
+    apellido: String;
+    img: String;
+
+    constructor(nombre: String, apellido: String, img: String)
+}{
+    this.nombre= nombre;
+    this.apellido= apellido;
+    this.img= img;
+}
