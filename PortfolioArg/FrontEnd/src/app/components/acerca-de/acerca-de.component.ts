4f20b2670257eb871022ba071f4575388e0ec29c
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-15 04:30:55 -0300

    acercaDe

--- PortfolioArg/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
+++ PortfolioArg/FrontEnd/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import {persona} from 'src/app/model/persona.model';
+import {PersonaService} from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-acerca-de',
   templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
+export class AcercaDeComponent implements OnInit {
+   persona: persona = new persona("","","");
 
+   constructor(public personaService: PersonaService) {}
+      ngOnInti(): void{
+        this.personaService.getPersona().subscribe(data =>{this.persona = data})
+      }
 }
