8047bdfc5b67ebe0d1d49a2cc5fe28f665eaff6b
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-15 04:31:36 -0300

    MgbApp

---
+++ PortfolioArg/BackEnd/src/main/java/com/portfoliolg/mgb/MgbApplication.java
@@ -0,0 +1,12 @@
+package com.portfoliolg.mgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgbApplication.class, args);
+	}
+}
