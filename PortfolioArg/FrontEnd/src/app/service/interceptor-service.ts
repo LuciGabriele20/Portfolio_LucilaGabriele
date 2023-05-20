53a976235ec61e3a7d877bb13a256e01063139d4
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-20 12:30:50 -0300

    service

---
+++ PortfolioArg/FrontEnd/src/app/service/interceptor-service.ts
@@ -0,0 +1,24 @@
+import { HTTP_INTERCEPTORS, HttpEvent,HttpHandler,HttpRequest } from "@angular/common/http"
+import { Observable } from "rxjs"
+import { TokenService } from "./token.service"
+
+export class InterceptorService{
+    constructor(private tokenServicie: TokenServise){}
+    
+    Intercept(req:HttpRequest<any>,next: HttpHandler): Observable<HttpEvent<any>>{
+        let intReq= req;
+        const token= this.tokenServicie.getToken();
+
+        if(token != null){
+            intReq =req.clone({
+                headers:req.headers.set(´'Authorization','Bearer'+token)
+            })
+        }
+        return next.handle(intReq);
+    }
+}
+export const interceptorProvider=[{
+    provide:HTTP_INTERCEPTORS,
+    useClass: InterceptorService,
+    multi:true
+}];
