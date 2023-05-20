ee1b6ec79f57334f7d4920d70fa4ab8aca8895e5
LuciGabriele20 <lucigabriele20@gmail.com>
2023-05-20 12:36:57 -0300

    iusuariorepo

---
+++ PortfolioArg/BackEnd/src/main/java/com/portfoliolg/mgb/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.portfoliolg.mgb.Security.Repository;
+
+import com.portfoliolg.mgb.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iUsuarioRepository  extends JpaRepository<Usuario, Integer>{
+   Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+   
+   boolean existsByNombreUsuario(String nombreUsuario);
+   boolean existsByEmail(String email);
+}
